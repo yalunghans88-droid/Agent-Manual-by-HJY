220d12dbbe456bc9411cecd2f422bd88f5706871
yalunghans88-droid <yalunghans88@gmail.com>
2025-10-11 21:53:28 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Optional: Navbar background on scroll
+window.addEventListener("scroll", function() {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.style.background = "#111";
+  } else {
+    navbar.style.background = "#0b0b0b";
+  }
+});
